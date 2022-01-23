a675b9eedd2c75b18552040ada91f25229463d25
Miguel Vargas <mavargas101@gmail.com>
2022-01-23 06:30:10 +0000

    Add comments to generateGrid

--- js/script.js
+++ js/script.js
@@ -14,17 +14,20 @@ generateGrid(20);
 function clearGrid(){
     grid.innerHTML= ''
 }
-
 function generateGrid(size){
+    //1.clear grid
+    //2.set template columns and rows to whatever size is
+    //3.create size**2 divs for the grid (gridling)
+    //3a. add the eventlistener to the gridling that will listen for a mouse hover to color in the div
+    //3b. append it to the grid 
     clearGrid();
     grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
     
-    for(let i = 0; i < (size*size); i++){
-        let pixel = document.createElement('div');
-        pixel.classList.add("pixel");
-        pixel.addEventListener('mouseover', () => hovered(pixel));
-        grid.appendChild(pixel);
+    for(let i = 0; i < size**2; i++){
+        let gridling = document.createElement('div');
+        gridling.addEventListener('mouseover', () => hovered(gridling));
+        grid.appendChild(gridling);
     }
 }
 
@@ -32,11 +35,8 @@ function hovered(element){
     element.style.background = "black";
 }
 
+//For the clear button
 function clear(){
-    let pixelList = grid.querySelectorAll('.pixel');
-    for(let i = 0; i < pixelList.length - 1; i++){
-        let item = pixelList[i]
-        item.style.background = "white";
-    }
-    console.log(rangeInput.value);
+    clearGrid();
+    generateGrid(rangeInput.value);
 }
